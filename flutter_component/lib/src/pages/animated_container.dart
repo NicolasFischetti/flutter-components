a82a36ed8fde00822f95aefdb757817bc252605c
Nicolas <nicolasf@lagash.com>
2019-10-10 14:12:24 -0300

    Add animated container component (not finish)

---
+++ flutter_component/lib/src/pages/animated_container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedContainerPage extends StatefulWidget {
+  @override
+  _AnimatedContainerPage createState() => _AnimatedContainerPage();
+}
+
+class _AnimatedContainerPage extends State<AnimatedContainerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
