9062bdf276b6af4dc28d5804492db44b9d1ed692
Nicolas <nicolasf@lagash.com>
2019-10-10 14:12:04 -0300

    Add avatar component

--- flutter_component/lib/src/pages/avatar_page.dart
+++ flutter_component/lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage("https://i.blogs.es/24bcec/the-batman-affleck/1366_2000.jpg"),
+              radius: 25.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text("BT"),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage("https://i.blogs.es/24bcec/the-batman-affleck/1366_2000.jpg"),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
